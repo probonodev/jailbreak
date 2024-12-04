061d3513b400e47975d113941cf8ee2677d641b3
probonodev <jailbreakcoin@proton.me>
2024-12-04 20:03:26 +0200

    HTML Metadta

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -8,16 +8,42 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Jailbreak",
-  description: "Jailbreak",
+  description:
+    "JailBreak is a decentralized platform where users are challenged to jailbreak LLMs to find weaknesses and Be rewarded.",
+  openGraph: {
+    title: "Jailbreak",
+    description:
+      "JailBreak is a decentralized platform where users are challenged to jailbreak LLMs to find weaknesses and Be rewarded.",
+    images: [
+      {
+        url: "./images/stoneLogo.png",
+        width: 800,
+        height: 800,
+        alt: "Jailbreak Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jailbreak",
+    description:
+      "JailBreak is a decentralized platform where users are challenged to jailbreak LLMs to find weaknesses and Be rewarded.",
+    images: ["./images/stoneLogo.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta property="og:image" content="./images/stoneLogo.png" />
+        <meta name="twitter:image" content="./images/stoneLogo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#ffffff" />
+
         <link rel="icon" href="./favicon.ico" sizes="any" />
         <link rel="icon" type="image/svg+xml" href="./favicon.ico" />
-        <link rel="apple-touch-icon" href="./favicon.ico" />
+        <link rel="apple-touch-icon" href="./images/stoneLogo.png" />
       </head>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
