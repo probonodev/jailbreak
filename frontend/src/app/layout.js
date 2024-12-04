7076a073ed400602694b5dd3ddbdf524d5aef292
probonodev <jailbreakcoin@proton.me>
2024-12-04 20:11:37 +0200

    HTML Metadta

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
       "JailBreak is a decentralized platform where users are challenged to jailbreak LLMs to find weaknesses and Be rewarded.",
     images: [
       {
-        url: "./images/stoneLogo.png",
+        url: "https://jailbreakme.xyz/images/stoneLogo.png",
         width: 800,
         height: 800,
         alt: "Jailbreak Logo",
@@ -36,7 +36,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta property="og:image" content="./images/stoneLogo.png" />
+        <meta
+          property="og:image"
+          content="https://jailbreakme.xyz/images/stoneLogo.png"
+        />
         <meta name="twitter:image" content="./images/stoneLogo.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#ffffff" />
